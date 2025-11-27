23cb0f41b92e0c59c45032baecd394ef3650ab5a
= <=>
2025-11-27 16:25:22 -0500

    Change the create_state for update function and steps 0, 1, 2

--- server_connection/flow/conversation_flow.js
+++ server_connection/flow/conversation_flow.js
@@ -1,4 +1,4 @@
-import { get_state,  get_employee, create_state} from "../APIs/api_client.js";
+import { get_state,  get_employee, create_state, get_services, update_state} from "../APIs/api_client.js";
 import { service_message } from "./service_message.js";
 
 export async function handle_conversation({user_phone, message_text, client}) {
@@ -7,20 +7,48 @@ export async function handle_conversation({user_phone, message_text, client}) {
         case -1:
             let message = `Hola! Bienvenido a ${client.business_name}\n`;
             const employees = await get_employee({id: client.id});
-
+            create_state({user_phone: user_phone, step: 0, client_id: client.id});
+            const state_id = state.user_state_id;
             if (employees.length > 1) {  //¿Multiple employees?
                 message += `¿Quién deseas que te atienda?\n`;
                 
                 employees.forEach((employee, index) => {
                     message += `${index + 1}. ${employee.name}\n`;
                 })
-                create_state({user_phone: user_phone, step: 0, client_id: client.id});
+                update_state({id: state_id, step: 0});
 
             } else if (employees.length === 1) { //Only one employee
                 message += `Serás atendido por ${employees[0].name}\n`;
-                message = await service_message({message, employee_id: employees[0].id, client_id: client.id, user_phone});
+                message = await service_message({message, employee_id: employees[0].id, client_id: client.id, user_phone, state_id});
+            }
+            
+        case 0:
+            const employees_list = await get_employee({id: client.id});
+            const emp_index = parseInt(message_text) - 1;
+            if (emp_index >= 0 && emp_index < employees_list.length) {
+                const selected_employee = employees_list[emp_index];
+                message = `Has seleccionado a ${selected_employee.name}.\n`;
+                message = await service_message({message, employee_id: selected_employee.id, client_id: client.id, user_phone, state_id});
+            }else{
+                message = `Por favor selecciona una opción válida.\n`;
+            }
+        
+        case 1:
+            const services = await get_services({client_id: client.id});
+            const serv_index = parseInt(message_text) - 1;
+            if (serv_index >= 0 && serv_index < services.length) {
+                const selected_service = services[serv_index];
+                message = `Has seleccionado el servicio: ${selected_service.name} por $${selected_service.price}.\n`;
+                message += `Deseas agregar algo más?\n1. Sí\n2. No\n`;
+                create_state({user_phone, step: 2, employee_id: state.employee_id, client_id: client.id});
+            }else{
+                message = `Por favor selecciona una opción válida.\n`;
             }
 
+        case 2:
+            if (message_text === '1') {
+                message = await service_message({message, employee_id: selected_employee.id, client_id: client.id, user_phone, state_id});
+            }
         return message;
     }
 }
--- server_connection/flow/service_message.js
+++ server_connection/flow/service_message.js
@@ -1,16 +1,20 @@
-import { get_services, create_state } from "../APIs/api_client.js";
+import { get_services, update_state } from "../APIs/api_client.js";
 
-export async function service_message({message, employee_id, client_id, user_phone}) {
+export async function service_message({message, employee_id, client_id, user_phone, state_id}) {
     const services = await get_services({client_id: client_id});
 
         if(services.length > 1) { //Multiple services
             
             message += `¿Qué servicio deseas?\n`;
-            services.forEach((service, index) => {
-                message += `${index + 1}. ${service.name}\n`;
+            services.forEach((service, index) => {  //TODO: poner precios
+                message += `${index + 1}. ${service.name}: $${service.price}\n`;
             })
 
-            create_state({user_phone, step: 1, employee_id, client_id: client_id});
+            update_state({id: state_id, step: 1, employee_selected: employee_id});
+        }else{
+            message += `El servicio disponible es: ${services[0].name} por $${services[0].price}.\n`;
+            //Continuar al siguiente paso directamente
+            update_state({id: state_id, step: 2, employee_selected: employee_id});
         }
 
     return message;
